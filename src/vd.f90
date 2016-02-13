aecc3772161b6082c23060053d110630dd893c69
Alex Kramer <kramer.alex.kramer@gmail.com>
2016-02-13 15:30:56 -0600

    VD module rewrite
    Signed-off-by: Alex Kramer <kramer.alex.kramer@gmail.com>

--- src/vd.f90
+++ src/vd.f90
@@ -1,250 +1,314 @@
 module vd
+  ! Virtual detector module
 
+  ! Imports -- intrinsic
   use ieee_arithmetic
 
+  ! Imports -- library dependencies
   use log, only: log_log_critical, log_stderr
   use numerics, only: numerics_cmplx_phase, numerics_d1, numerics_d2, &
-       numerics_trapz
+       numerics_trapz, numerics_linspace
   use dists, only: dists_gaussian
 
-  use progvars
-  use output, only: output_logfile_unit
+  ! Imports -- program variables
+  use progvars, only: fp, ip, hbar, m
 
   implicit none
 
   private
 
-  public :: vd_init
-  public :: vd_cleanup
-  public :: vd_normalize
-  public :: vd_update
+  public :: vd_obj
+  type vd_obj
+     private
 
-  integer(ip), parameter :: logfile_unit = output_logfile_unit
+     logical :: semi_classical
 
-  ! Work arrays
-  real(fp), allocatable :: phi_arr(:), p_arr(:)
-  real(fp), allocatable :: p_var_arr(:)
-  real(fp), allocatable :: mag_arr(:), j_arr(:)
-  real(fp), allocatable :: prob_arr(:)
+     real(fp) :: dx
+     integer(ip) :: nxl, nxr
+     integer(ip) :: xl_min, xl_max
+     integer(ip) :: xr_min, xr_max
+
+     integer(ip) :: np
+     real(fp) :: p_min, p_max
+     real(fp) :: dp
+     real(fp), allocatable :: p_range(:)
+     real(fp), allocatable :: vd_p_arr(:)
+
+     real(fp) :: dt
+
+     real(fp), allocatable :: phi_arr_l(:), phi_arr_r(:)
+     real(fp), allocatable :: p_arr_l(:), p_arr_r(:)
+     real(fp), allocatable :: p_var_arr_l(:), p_var_arr_r(:)
+     real(fp), allocatable :: mag_arr_l(:), mag_arr_r(:)
+     real(fp), allocatable :: j_arr_l(:), j_arr_r(:)
+
+   contains
+     procedure :: init => vd_init
+     procedure :: cleanup => vd_cleanup
+     procedure :: normalze => vd_normalize
+     procedure :: update => vd_update
+     procedure :: bin => vd_bin
+     procedure :: fill_arrays => vd_fill_arrays
+  end type vd_obj
 
 contains
 
-  subroutine vd_init()
-    logical :: sane
+  subroutine vd_init(this, nx, nxl_ext, nxr_ext, nxl_vd, nxr_vd, dx, np, &
+       p_min, p_max, dt, sc)
+    ! Initialize VD object
+    class(vd_obj), intent(inout) :: this
+    integer(ip), intent(in) :: nx
+    integer(ip), intent(in) :: nxl_ext
+    integer(ip), intent(in) :: nxr_ext
+    integer(ip), intent(in) :: nxl_vd
+    integer(ip), intent(in) :: nxr_vd
+    real(fp), intent(in) :: dx
+    integer(ip), intent(in) :: np
+    real(fp), intent(in) :: p_min
+    real(fp), intent(in) :: p_max
+    real(fp), intent(in) :: dt
+    logical, intent(in) :: sc
 
+    logical :: sane
     character(:), allocatable :: error_msg
 
-    ! Check internal / external / virtual detector grid limits
-
-    sane = .true.
-
-    if (nx .le. nxl_external + nxr_external) then
-       sane = .false.
-    else if (nxl_external .le. vd_nxl) then
-       sane = .false.
-    else if (nxr_external .le. vd_nxr) then
-       sane = .false.
-    end if
-
+    ! Validate spatial grid parameters
+    sane = (nx .lt. nxl_ext + nxr_ext + nxl_vd + nxr_vd)
     if (.not. sane) then
-       error_msg = "Error in numerical / virtual detector grid "// &
-            "configuration; exiting abnormally."
-       call log_log_critical(error_msg, logfile_unit)
+       error_msg = "Error in numerical grid; stopping abnormally"
        call log_log_critical(error_msg, log_stderr)
+       deallocate(error_msg)
        stop 0
     end if
 
-    ! Calculate edges of VD grid
-    ! The +/- 1 terms here are because Fortran uses 1-based arrays
-    vd_xl_min = nxl_external - vd_nxl + 1
-    vd_xl_max = nxl_external
-    vd_xr_min = nx - nxr_external
-    vd_xr_max = vd_xr_min + vd_nxr - 1
+    ! Validate momentum grid parameters
+    sane = (p_min .lt. p_max)
+    if (.not. sane) then
+       error_msg = "Error in momentum grid; stopping abnormally"
+       call log_log_critical(error_msg, log_stderr)
+       deallocate(error_msg)
+       stop 0
+    end if
 
-    ! Allocate work arrays
-    allocate(phi_arr(nx))
-    allocate(p_arr(nx))
-    allocate(p_var_arr(nx))
-    allocate(mag_arr(nx))
-    allocate(j_arr(nx))
-    allocate(prob_arr(nx))
+    ! Assign VD scalar parameters
+    this%dx = dx
+    this%nxl = nxl_vd
+    this%nxr = nxr_vd
+
+    this%np = np
+    this%p_min = p_min
+    this%p_min = p_max
+
+    this%dt = dt
+
+    this%semi_classical = sc
+
+    ! Allocate and construct VD momentum grid
+    allocate(this%vd_p_arr(np))
+    allocate(this%p_range(np))
+    call numerics_linspace(p_min, p_max, this%p_range, this%dp)
+
+    ! Calculate VD grid edge indices on total spatial grid
+    this%xl_min = nxl_ext
+    this%xl_max = this%xl_min + nxl_vd
+    this%xr_max = nx - nxr_ext
+    this%xr_min = this%xr_max - nxr_vd
+
+    ! Allocate internal work arrays
+    allocate(this%phi_arr_l(0 : nxl_vd + 1), this%phi_arr_r(0 : nxr_vd + 1))
+    allocate(this%p_arr_l(0 : nxl_vd + 1), this%p_arr_r(0 : nxr_vd + 1))
+    allocate(this%p_var_arr_l(0 : nxl_vd + 1), &
+         this%p_var_arr_r(0 : nxr_vd + 1))
+    allocate(this%mag_arr_l(0 : nxl_vd + 1), this%mag_arr_r(0 : nxl_vd + 1))
+    allocate(this%j_arr_l(0 : nxl_vd + 1), this%j_arr_r(0 : nxl_vd + 1))
 
   end subroutine vd_init
 
-  subroutine vd_cleanup
-    ! Deallocate work arrays
+  subroutine vd_cleanup(this)
+    ! Deallocate all object arrays
+    class(vd_obj), intent(inout) :: this
 
-    deallocate(prob_arr)
-    deallocate(j_arr)
-    deallocate(mag_arr)
-    deallocate(p_var_arr)
-    deallocate(p_arr)
-    deallocate(phi_arr)
+    deallocate(this%phi_arr_l, this%phi_arr_r)
+    deallocate(this%p_arr_l, this%p_arr_r)
+    deallocate(this%p_var_arr_l, this%p_var_arr_r)
+    deallocate(this%mag_arr_l, this%mag_arr_r)
+    deallocate(this%j_arr_l, this%j_arr_r)
 
+    deallocate(this%vd_p_arr)
   end subroutine vd_cleanup
 
-  subroutine vd_normalize(np_arr)
+  subroutine vd_normalize(this)
     ! Normalize virtual detector spectrum
-    real(fp), intent(inout) :: np_arr(:)
+    class(vd_obj), intent(inout) :: this
+
     real(fp) :: np_norm
 
-    np_norm = numerics_trapz(np_arr, vd_dp)
-    np_arr(:) = np_arr(:) / np_norm
-
+    np_norm = numerics_trapz(this%vd_p_arr, this%dp)
+    this%vd_p_arr(:) = this%vd_p_arr(:) / np_norm
   end subroutine vd_normalize
 
-  subroutine vd_update(psi_arr, np_arr)
-    ! Update virtual detector
-    complex(fp), intent(in) :: psi_arr(:)
-    real(fp), intent(inout) :: np_arr(:)
-
-    call vd_fill_arrays(psi_arr)
-    call vd_calc_pj()
-    call vd_bin(np_arr)
-
-  end subroutine vd_update
-
-  subroutine vd_bin(np_arr)
-    real(fp), intent(inout) :: np_arr(:)
+  subroutine vd_bin(this)
+    ! Update virtual detector counts
+    class(vd_obj), intent(inout) :: this
 
     integer(ip) :: i_x
 
-    do i_x = vd_xl_min, vd_xl_max
-       call accumulate_counts(i_x, np_arr)
+    ! Accumulate counts along left virtual detector grid
+    do i_x = this%xl_min, this%xl_max
+       call accumulate_counts(i_x, this%p_arr_l, this%p_var_arr_l, &
+            this%j_arr_l)
     end do
-    do i_x = vd_xr_min, vd_xr_max
-       call accumulate_counts(i_x, np_arr)
+
+    ! Accumulate counts along right virtual detector grid
+    do i_x = this%xr_min, this%xr_max
+       call accumulate_counts(i_x, this%p_arr_r, this%p_var_arr_r, &
+            this%j_arr_r)
     end do
 
   contains
 
-    subroutine accumulate_counts(i_x, count_arr)
+    subroutine accumulate_counts(i_x, p_arr, p_var_arr, j_arr)
+      ! Method that actually updates virtual detector counts
+      !
+      ! i_x :: spatial grid index
+      ! p_arr :: local momentum array, indexed by i_x
+      ! p_var_arr :: local variance array, indexed by i_x
+      ! j_arr :: local current array, indexed by i_x
       integer(ip), intent(in) :: i_x
-      real(fp), intent(inout) :: count_arr(:)
+      real(fp), intent(in) :: p_arr(:)
+      real(fp), intent(in) :: p_var_arr(:)
+      real(fp), intent(in) :: j_arr(:)
 
-      ! Semi-classical case uses delta-function histogramming
-      ! We mimic this by using a Gaussian with variation vd_dp / 5, so that
-      ! roughly 100% of the binned distribution will be located at a single
-      ! grid point.
-
-      real(fp) :: p, p_mu, p_var, p_var_sc
-      real(fp) :: scale, g
       integer(ip) :: i_p
-      ! mean
+      integer(ip) :: i_p_sc
+
+      real(fp) :: p
+      real(fp) :: p_mu
+      real(fp) :: p_var
+
+      real(fp) :: scale
+      real(fp) :: g
+
+      ! mean momentum
       p_mu = p_arr(i_x)
-      ! variance
+      ! momentum variance
       p_var = p_var_arr(i_x)
 
-      ! semiclassical variance
-      p_var_sc = vd_dp / 16
+      ! semi-classical adjustments
+      ! The semi-classical case uses delta-function histogramming, which we
+      ! mimic by using a Gaussian with variance dp/4 so that roughly 100% of
+      ! the binned distribution will be located at a single grid point.
+      if (this%semi_classical) then
+         ! shift p_mu to be at the nearest grid point
+         i_p_sc = nint( (p_mu - this%p_min) / this%dp )
+         p_mu = this%p_min + this%dp * i_p_sc
 
-      if (vd_semi_classical) then
-         p_var = p_var_sc
+         p_var = this%dp / 16
       end if
 
-      ! Make Gaussian of variance p_var around p, and populate momentum
-      ! distribution using that
-      scale = dt * abs(j_arr(i_x))
+      scale = this%dt * abs(j_arr(i_x))
 
-      do i_p = 1, vd_np
-         p = vd_p_range(i_p)
+      ! Make gaussian of variance p_var around p_mu, and population momentum
+      ! distribution using that.
+      do i_p = 1, this%np
+         p = this%p_range(i_p)
          g = dists_gaussian(p, p_mu, p_var)
-         count_arr(i_p) = count_arr(i_p) + scale * g
+         this%vd_p_arr(i_p) = this%vd_p_arr(i_p) + scale * g
       end do
 
     end subroutine accumulate_counts
 
   end subroutine vd_bin
 
-  subroutine vd_calc_pj()
-
-    real(fp) :: p_var, eps_fp
-    integer(ip) :: i_x
-
-    ! Calculate p_local = d(phi) / dx
-    call numerics_d1(phi_arr(vd_xl_min - 1 : vd_xl_max + 1), &
-         p_arr(vd_xl_min - 1 : vd_xl_max + 1), dx)
-    call numerics_d1(phi_arr(vd_xr_min - 1 : vd_xr_max + 1), &
-         p_arr(vd_xr_min - 1 : vd_xr_max + 1), dx)
-
-    ! Calculate second moment of local momentum dist.
-    ! From Iafrate, G., et al. Journal de Physique 42.C7.10 (1981).
-    ! Note that this is numerically unstable! Right now we check for this and
-    ! manually attempt to correct for this, but if we really want to do this
-    ! correctly, we should look into using more stable derivative methods or
-    ! an alternative variance measure to avoid this issue.
-    call numerics_d2(log(mag_arr(vd_xl_min - 1 : vd_xl_max + 1)), &
-         p_var_arr(vd_xl_min - 1 : vd_xl_max + 1), dx)
-    call numerics_d2(log(mag_arr(vd_xr_min - 1 : vd_xr_max + 1)), &
-         p_var_arr(vd_xr_min - 1 : vd_xr_max + 1), dx)
-
-    p_var_arr(:) = - hbar**2 / 4.0_fp * p_var_arr(:)
-
-    ! There are cases in which the values of mag_arr are zero (below machine
-    ! precision so that the derivative operation above is completely unstable.
-    ! In these cases, we solve this by setting the variance equal to the
-    ! machine epsilon. This can be justified (in some sense) using L'Hopital's
-    ! rule:
+  subroutine vd_update(this, psi_arr)
+    ! Update virtual detector counts
     !
-    ! d^2/dx^2 log(rho) = 1/rho^2 ( rho * rho'' - rho'^2)
+    ! this :: vd_obj instance
+    ! psi_arr :: 1D slice of wavefunction along the same grid as vd_obj
     !
-    ! If rho <= eps then rho' and rho'' <= rho
-    ! so d^2/dx^2 log(rho) <= eps
-    !
-    ! We also correct for negative variances here, which are almost certainly
-    ! due to numerical accuracy issues.
-    eps_fp = epsilon(1.0_fp)
-    do i_x = vd_xl_min, vd_xl_max
-       p_var = p_var_arr(i_x)
-       if (ieee_is_nan(p_var) .or. (p_var .lt. eps_fp)) then
-          p_var_arr(i_x) = eps_fp
-       end if
-    end do
-
-    do i_x = vd_xr_min, vd_xr_max
-       p_var = p_var_arr(i_x)
-       if (ieee_is_nan(p_var) .or. (p_var .lt. eps_fp)) then
-          p_var_arr(i_x) = eps_fp
-       end if
-    end do
-
-    ! Calculate j_local = rho * p_local / m
-    j_arr(vd_xl_min : vd_xl_max) = mag_arr(vd_xl_min : vd_xl_max) / m * &
-         p_arr(vd_xl_min : vd_xl_max)
-    j_arr(vd_xr_min : vd_xr_max) = mag_arr(vd_xr_min : vd_xr_max) / m * &
-         p_arr(vd_xr_min : vd_xr_max)
-
-  end subroutine vd_calc_pj
-
-  subroutine vd_fill_arrays(psi_arr)
-    ! Calculate R^2(x,t), S(x,t) where Psi = R exp(i S / hbar)
+    ! For multi-dimensional propagations, this routine should be called for
+    ! each slice along the grid of vd_obj
+    class(vd_obj), intent(inout) :: this
     complex(fp), intent(in) :: psi_arr(:)
 
-    complex(fp) :: z
+    call this%fill_arrays(psi_arr)
+    call this%bin()
+  end subroutine vd_update
+
+  subroutine vd_fill_arrays(this, psi_arr)
+    class(vd_obj), intent(inout) :: this
+    complex(fp), intent(in) :: psi_arr(:)
+
     integer(ip) :: i_x
+    integer(ip) :: i_x_vd
+    complex(fp) :: z
+    real(fp) :: eps_fp
+    real(fp) :: p_var
 
-    ! Left end of VD grid
-    do i_x = vd_xl_min - 1, vd_xl_max + 1
-       call fill_by_index(i_x)
+    eps_fp = epsilon(1.0_fp)
+
+    ! left end of VD grid
+    do i_x = this%xl_min - 1, this%xl_max + 1
+       ! i_x indexes the total spatial grid; we need to convert this to the
+       ! internal grids we use.
+       i_x_vd = i_x - (this%xl_min - 1)
+
+       z = psi_arr(i_x)
+       this%phi_arr_l(i_x_vd) = numerics_cmplx_phase(z) * hbar
+       this%mag_arr_l(i_x_vd) = abs(z)**2
     end do
 
-    ! Right end of VD grid
-    do i_x = vd_xr_min - 1, vd_xr_max + 1
-       call fill_by_index(i_x)
+    call numerics_d1(this%phi_arr_l(:), this%p_arr_l(:), this%dx)
+    this%j_arr_l(:) = this%mag_arr_l(:) / m * this%p_arr_l(:)
+
+    ! Calculate variance of local momentum distribution.
+    ! Note that this is numerically unstable! Right now, we check for this and
+    ! manually attempt to correct for it, but we should think about finding
+    ! more stable methods to calculate this or find some alternative measure of
+    ! variance.
+    call numerics_d2(log(this%mag_arr_l(:)), this%p_var_arr_l(:), this%dx)
+    this%p_var_arr_l(:) = -hbar**2 / 4.0_fp * this%p_var_arr_l(:)
+
+    ! There are cases in which the values of mag_arr are less than machine
+    ! precision so that the derivative operation above is completely
+    ! numerically unstable. In these cases, we attempt to solve this by setting
+    ! the variance equal to the machine epsilon. This can be justified
+    ! (informally) using L'Hopital's rule:
+    !
+    ! d^2/dx^2 log(rho) = 1 / rho^2 ( rho * rho'' - rho''^2)
+    !
+    ! If rho <= eps locally then rho' and rho'' <= rho, so
+    ! d^2/dx^2 log(rho) <= eps, which means we can't have a better estimate
+    ! than setting p_var to be the machine epsilon.
+    do i_x_vd = 0, this%nxl + 1
+       p_var = this%p_var_arr_l(i_x_vd)
+       if (ieee_is_nan(p_var) .or. (p_var .lt. eps_fp)) then
+          this%p_var_arr_l(i_x_vd) = eps_fp
+       end if
     end do
 
-  contains
+    ! right side of VD grid -- same procedure as the left side above
+    do i_x = this%xr_min - 1, this%xr_max + 1
+       i_x_vd = i_x - (this%xr_min - 1)
 
-    subroutine fill_by_index(i_x)
-      integer(ip), intent(in) :: i_x
+       z = psi_arr(i_x)
+       this%phi_arr_r(i_x_vd) = numerics_cmplx_phase(z) * hbar
+       this%mag_arr_r(i_x_vd) = abs(z)**2
+    end do
 
-      z = psi_arr(i_x)
+    call numerics_d1(this%phi_arr_r(:), this%p_arr_r(:), this%dx)
+    this%j_arr_r(:) = this%mag_arr_r / m * this%p_arr_r(:)
 
-      ! multiply by hbar here since Psi ~ exp(i S / hbar)
-      phi_arr(i_x) = numerics_cmplx_phase(z) * hbar
-      mag_arr(i_x) = abs(z)**2
+    call numerics_d2(log(this%mag_arr_r(:)), this%p_var_arr_r(:), &
+         this%dx)
+    this%p_var_arr_r(:) = -hbar**2 / 4.0_fp * this%p_var_arr_r(:)
 
-    end subroutine fill_by_index
+    do i_x_vd = 0, this%nxr + 1
+       p_var = this%p_var_arr_r(i_x_vd)
+       if (ieee_is_nan(p_var) .or. (p_var .lt. eps_fp)) then
+          this%p_var_arr_r(i_x_vd) = eps_fp
+       end if
+    end do
 
   end subroutine vd_fill_arrays
 
